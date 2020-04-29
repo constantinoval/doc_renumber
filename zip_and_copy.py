5376c1bd730c27cdb8177da446ad39f8900542b2
Aleksandr Konstantinov <constantinov.al@yandex.ru>
2020-04-29 19:12:13 +0300

    zip and copy script to make single pyz file

---
+++ zip_and_copy.py
@@ -0,0 +1,18 @@
+from zipfile import ZipFile
+from pathlib import Path
+from shutil import copyfile
+
+
+file_path = Path() / 'doc_renumber.pyz'
+z = ZipFile(file_path, 'w')
+for f in ['doctools_lib.py',
+          'iter_blocks.py',
+          'renumber_figures.py',
+          'renumber_formulas.py',
+          'renumber_literature.py',
+          'renumber_tables.py',
+          '__main__.py'
+          ]:
+    z.write(f, f)
+z.close()
+copyfile(file_path, 'd:/programs/doc_renumber.pyz')
