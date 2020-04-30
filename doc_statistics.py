c7efb66924da457f5d5da2ca88ca40c47a37d2d1
Aleksandr Konstantinov <constantinov.al@yandex.ru>
2020-05-01 02:19:19 +0300

    doc_statistics optimization

--- doc_statistics.py
+++ doc_statistics.py
@@ -1,24 +1,96 @@
-from renumber_figures import analize_figures
-from renumber_formulas import analize_formulas
-from renumber_tables import analize_tables
+import re
+from doctools_lib import paragraph_iterator
 from docx import Document
-from colorama import Fore, init
+from colorama import Fore, init, Back
 from pprint import pprint
+
+
+def analize_figures(lines):
+    stop_tag = re.compile(r'<f+stop>')
+    continue_tag = re.compile(r'<f+continue>')
+    pat = re.compile(
+        r'^\s*(Рис\.|Рисунок)\s+(?P<num>[\d.]+?)\s*([–-].*|[. ]*$)')
+    rez = []
+    do_analysis = True
+    for p in lines:
+        if stop_tag.search(p):
+            do_analysis = False
+        if continue_tag.search(p):
+            do_analysis = True
+        if not do_analysis:
+            continue
+        m = pat.match(p)
+        if m:
+            rez.append(m.group('num'))
+    return rez
+
+
+def analize_formulas(lines):
+    stop_tag = re.compile(r'<e+stop>')
+    continue_tag = re.compile(r'<e+continue>')
+    pat = re.compile(r'[^а-яА-Я]*\((?P<num>[\d.-]+)\)\s*$')
+    rez = []
+    do_analysis = True
+    for p in lines:
+        if stop_tag.search(p):
+            do_analysis = False
+        if continue_tag.search(p):
+            do_analysis = True
+        if not do_analysis:
+            continue
+        m = pat.match(p)
+        if m:
+            rez.append(m.group('num'))
+    return rez
+
+
+def analize_tables(lines):
+    stop_tag = re.compile(r'<t+stop>')
+    continue_tag = re.compile(r'<t+continue>')
+    pat = re.compile(
+        r'^\s*(Таб\.|Таблица|Табл.)\s+(?P<num>[\d.]+?)\s*([–-].*|[. ]*$)')
+    rez = []
+    do_analysis = True
+    for p in lines:
+        if stop_tag.search(p):
+            do_analysis = False
+        if continue_tag.search(p):
+            do_analysis = True
+        if not do_analysis:
+            continue
+        m = pat.match(p)
+        if m:
+            rez.append(m.group('num'))
+    return rez
+
+
+def get_lines(doc):
+    rez = []
+    for p in paragraph_iterator(doc):
+        rez.append(p.text)
+    return rez
 
 
 def doc_analysis(doc):
     init(autoreset=True)
-    d = Document(doc)
-    f = analize_figures(d)
+    print(Fore.BLACK+Back.WHITE+'Reading text...')
+    lines = get_lines(d)
+    print(Fore.BLACK+Back.WHITE+'Done...')
+    f = analize_figures(lines)
     print(Fore.GREEN + 'Figures:', len(f))
-    pprint(list(f.keys()), indent=5, width=40, compact=True)
-    e = analize_formulas(d)
+    pprint(f, indent=5, width=40, compact=True)
+    e = analize_formulas(lines)
     print(Fore.GREEN + 'Equations:', len(e))
-    pprint(list(e.keys()), indent=5, width=40, compact=True)
-    t = analize_tables(d)
+    pprint(e, indent=5, width=40, compact=True)
+    t = analize_tables(lines)
     print(Fore.GREEN + 'Tables:', len(t))
-    pprint(list(t.keys()), indent=5, width=40, compact=True)
+    pprint(t, indent=5, width=40, compact=True)
+
+    print(Back.YELLOW+Fore.BLACK + 'Figures:'+Fore.WHITE+'\t'+str(len(f)))
+    print(Back.YELLOW+Fore.BLACK + 'Equations:'+Fore.WHITE+'\t'+str(len(e)))
+    print(Back.YELLOW+Fore.BLACK + 'Tables:'+Fore.WHITE+'\t'+str(len(t)))
 
 
 if __name__ == '__main__':
-    doc_analysis(r"D:\oneDrive\work\НИИМ\НИАГАРА\Отчет по 218\all\2.docx")
+    d = Document(r"D:\oneDrive\work\НИИМ\НИАГАРА\Отчет по 218\all\2.docx")
+    doc_analysis(d)
